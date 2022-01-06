ce43d89fce68c6efe353fa568040945e3bbb1116
Jonathan Dawson <jonathan.dawson@cognizant.com>
2022-01-06 10:17:10 -0600

    create employee class

---
+++ src/com/javapractice/Employee.java
@@ -0,0 +1,30 @@
+package com.javapractice;
+
+public class Employee {
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    long id;
+    String name;
+
+    public Employee(){
+
+    }
+    public Employee(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
