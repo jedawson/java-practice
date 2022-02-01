0d22c963f3b18910ad28e2c8bc69b16f3e829fe5
Jonathan Dawson <dawsonjed@gmail.com>
2022-02-01 09:33:54 -0600

    backend commit after figuring out jdk issue.

---
+++ amsbackend/src/main/java/org/example/amsbackend/AmsbackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.amsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmsbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmsbackendApplication.class, args);
+	}
+
+}
---
+++ amsbackend/src/test/java/org/example/amsbackend/AmsbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.amsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmsbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
