b5988e90c955b3d25bc9feac931164bc8eaf9636
Jonathan Dawson <dawsonjed@gmail.com>
2022-02-03 06:34:12 -0600

    create state

--- amsfrontend/src/routes/BankerLogin.tsx
+++ amsfrontend/src/routes/BankerLogin.tsx
@@ -1,6 +1,32 @@
+import { useState } from "react";
+
+export interface Input {
+    username: any;
+    password: any;
+}
+
 const BankerLogin = () => {
+    const [input, setInput] = useState<Input>({username: '', password: ''})
     return (
-        <h1>If you are a banker, please login. If not, go back.</h1>
+        <div id="loginForm">
+            <h1>If you are a banker, please login. If not, go back.</h1>
+            <form>
+                <input 
+                    name="username"
+                    type="text"
+                    placeholder="Username"
+                    value={input.username}
+                    />
+                <input 
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    />
+                <button type="submit">Login</button>
+            </form>
+            
+        </div>
+        
     )
 }
 
