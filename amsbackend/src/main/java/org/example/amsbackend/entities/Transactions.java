53bb3fa57eb8f4e3db8fd9fe03f3c3a42c974943
Jonathan Dawson <dawsonjed@gmail.com>
2022-02-15 13:12:55 -0600

    create transactions entity

---
+++ amsbackend/src/main/java/org/example/amsbackend/entities/Transactions.java
@@ -0,0 +1,80 @@
+package org.example.amsbackend.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Transactions {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    int id;
+    int transactionRefNum;
+    Date time;
+    String debitOrCredit;
+    String cashOrTransfer;
+    double balance;
+
+    public Transactions() {
+    }
+
+    public Transactions(int id, int transactionRefNum, Date time, String debitOrCredit, String cashOrTransfer, double balance) {
+        this.id = id;
+        this.transactionRefNum = transactionRefNum;
+        this.time = time;
+        this.debitOrCredit = debitOrCredit;
+        this.cashOrTransfer = cashOrTransfer;
+        this.balance = balance;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTransactionRefNum() {
+        return transactionRefNum;
+    }
+
+    public void setTransactionRefNum(int transactionRefNum) {
+        this.transactionRefNum = transactionRefNum;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public String getDebitOrCredit() {
+        return debitOrCredit;
+    }
+
+    public void setDebitOrCredit(String debitOrCredit) {
+        this.debitOrCredit = debitOrCredit;
+    }
+
+    public String getCashOrTransfer() {
+        return cashOrTransfer;
+    }
+
+    public void setCashOrTransfer(String cashOrTransfer) {
+        this.cashOrTransfer = cashOrTransfer;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
