3dbc436347e9a9adfebe3c50f0b1f123a7696283
Jonathan Dawson <dawsonjed@gmail.com>
2022-02-15 13:05:28 -0600

    add request body annotation

--- amsbackend/src/main/java/org/example/amsbackend/controllers/AccountController.java
+++ amsbackend/src/main/java/org/example/amsbackend/controllers/AccountController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -17,5 +18,5 @@ public class AccountController {
 
     @CrossOrigin(origins = "*")
     @PostMapping("/accounts/all")
-    public List<Accounts> getAllByPan(Integer pan) {return this.accountService.findAllByPan(pan);}
+    public List<Accounts> getAllByPan(@RequestBody Integer pan) {return this.accountService.findAllByPan(pan);}
 }
